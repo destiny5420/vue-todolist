3636a71133e3dedf0d4a4c20f23590ed01b20831
paper.window <destiny5420@gmail.com>
2020-08-12 01:06:01 +0800

    [Add] 新增正確的樣式表

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import Vue from "vue";
-import App from "./App.vue";
+import App from "@/views/vApp/index.vue";
 import router from "./router";
 import store from "./store";
+
+// Font Awesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faUserSecret,
+  faList,
+  faCalendarCheck
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faUserSecret, faList, faCalendarCheck);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
